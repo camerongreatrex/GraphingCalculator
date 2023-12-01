8d4a89d9e62c3a54937a42080b4f011a9000324a
camerongreatrex <chuckify.outguild@gmail.com>
2023-11-30 21:30:33 -0500

    undo broken stuff when formatting

--- src/main/java/com/crescent/graphingcalculator/GraphingCalculator.java
+++ src/main/java/com/crescent/graphingcalculator/GraphingCalculator.java
@@ -31,42 +31,84 @@ public class GraphingCalculator extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Graphing Calculator");
 
+
+        //TODO
+            //fix scaling on the axis
+            //make points hoverable
+            //fix some labeling on the axis dissapearing along with random axis lines drawn
+
+
         mainPane = new Pane();
         primaryStage.setResizable(false);
 
-        // create axis for the chart
+        // Create the axis
         NumberAxis xAxis = new NumberAxis();
         NumberAxis yAxis = new NumberAxis();
         xAxis.setLabel("x");
         yAxis.setLabel("y");
 
-        // create the line chart
+        // Create the line chart
         chart = new LineChart<>(xAxis, yAxis);
         chart.setLayoutX(20);
-        chart.setLayoutY(150);
+        chart.setLayoutY(150); // Adjusted layout for graph to avoid overlap
         chart.setPrefSize(650, 650);
         chart.setLegendVisible(false);
 
-        // initialize text fields, radio buttons, and buttons
-        setUpFields();
-        setUpRadioButtons();
-        setUpButtons();
+        // Text fields for slope, intercept, and constant
+        slopeField = new TextField();
+        slopeField.setLayoutX(20);
+        slopeField.setLayoutY(20);
+        slopeField.setPrefWidth(180); // Adjusted width for text fields
 
-        // add nodes to the main pane
+        interceptField = new TextField();
+        interceptField.setLayoutX(20);
+        interceptField.setLayoutY(60);
+        interceptField.setPrefWidth(180); // Adjusted width for text fields
+
+        constantField = new TextField();
+        constantField.setLayoutX(20);
+        constantField.setLayoutY(100);
+        constantField.setPrefWidth(180); // Adjusted width for text fields
+
+        // Radio buttons for selecting function type
+        linearRadioButton = new RadioButton("Linear Function");
+        linearRadioButton.setSelected(true);
+        linearRadioButton.setLayoutX(250);
+        linearRadioButton.setLayoutY(20);
+
+        parabolaRadioButton = new RadioButton("Parabola");
+        parabolaRadioButton.setLayoutX(250);
+        parabolaRadioButton.setLayoutY(60);
+
+        ToggleGroup functionToggleGroup = new ToggleGroup();
+        linearRadioButton.setToggleGroup(functionToggleGroup);
+        parabolaRadioButton.setToggleGroup(functionToggleGroup);
+
+        // Button to plot the graph
+        Button plotGraphButton = new Button("Plot Graph");
+        plotGraphButton.setLayoutX(450);
+        plotGraphButton.setLayoutY(20);
+        plotGraphButton.setOnAction(e -> plotGraph());
+
+        // Button to reset the graph
+        Button resetButton = new Button("Reset Graph");
+        resetButton.setLayoutX(450);
+        resetButton.setLayoutY(60);
+        resetButton.setOnAction(e -> resetGraph());
+
         mainPane.getChildren().addAll(chart, slopeField, interceptField, constantField, linearRadioButton,
-                parabolaRadioButton);
+                parabolaRadioButton, plotGraphButton, resetButton);
 
-        // create the scene and set it to the stage
-        Scene scene = new Scene(mainPane, 800, 800);
+        Scene scene = new Scene(mainPane, 800, 800); // Adjusted scene height
         primaryStage.setScene(scene);
 
-        // set initial text prompt and visibility for constantField
+        // Set initial prompt texts based on the selected function type
         slopeField.setPromptText("Enter slope (m)");
         interceptField.setPromptText("Enter y-intercept (b)");
         constantField.setVisible(false);
         constantField.setPromptText("Enter constant (a)");
 
-        // define actions for radio button selection changes
+        constantField.setVisible(false);
         linearRadioButton.setOnAction(e -> {
             constantField.setVisible(false);
             resetGraph();
@@ -79,65 +121,7 @@ public class GraphingCalculator extends Application {
         primaryStage.show();
     }
 
-    // initialize text fields for slope, intercept, and constant
-    private void setUpFields() {
-        slopeField = createTextField(20, 20);
-        interceptField = createTextField(20, 60);
-        constantField = createTextField(20, 100);
-        constantField.setVisible(false);
-    }
-
-    // create a text field with given layout coordinates
-    private TextField createTextField(double layoutX, double layoutY) {
-        TextField textField = new TextField();
-        textField.setLayoutX(layoutX);
-        textField.setLayoutY(layoutY);
-        textField.setPrefWidth(180);
-        mainPane.getChildren().add(textField);
-        return textField;
-    }
-
-    // initialize radio buttons for linear and parabolic functions
-    private void setUpRadioButtons() {
-        linearRadioButton = createRadioButton("Linear Function", 250, 20, true);
-        parabolaRadioButton = createRadioButton("Parabola", 250, 60, false);
-
-        ToggleGroup functionToggleGroup = new ToggleGroup();
-        linearRadioButton.setToggleGroup(functionToggleGroup);
-        parabolaRadioButton.setToggleGroup(functionToggleGroup);
-    }
-
-    // create a radio/option button with given text, layout coordinates, and selection status
-    private RadioButton createRadioButton(String text, double layoutX, double layoutY, boolean selected) {
-        RadioButton radioButton = new RadioButton(text);
-        radioButton.setLayoutX(layoutX);
-        radioButton.setLayoutY(layoutY);
-        radioButton.setSelected(selected);
-        mainPane.getChildren().add(radioButton);
-        return radioButton;
-    }
-
-    // initialize buttons for plot and reset functionalities
-    private void setUpButtons() {
-        Button plotGraphButton = createButton("Plot Graph", 450, 20);
-        plotGraphButton.setOnAction(e -> plotGraph());
-
-        Button resetButton = createButton("Reset Graph", 450, 60);
-        resetButton.setOnAction(e -> resetGraph());
-
-        mainPane.getChildren().addAll(plotGraphButton, resetButton);
-    }
-
-    // create a button with given text, layout coordinates, and add it to the main pane
-    private Button createButton(String text, double layoutX, double layoutY) {
-        Button button = new Button(text);
-        button.setLayoutX(layoutX);
-        button.setLayoutY(layoutY);
-        mainPane.getChildren().add(button);
-        return button;
-    }
-
-    // plot the selected graph type (linear or parabolic)
+    // Method to plot the selected graph (linear or parabolic)
     private void plotGraph() {
         try {
             if (linearRadioButton.isSelected()) {
@@ -146,21 +130,24 @@ public class GraphingCalculator extends Application {
                 plotParabola();
             }
         } catch (NumberFormatException ex) {
+            // Handle invalid input
             Label errorLabel = new Label("Please enter valid numbers for coefficients.");
             errorLabel.setLayoutX(20);
-            errorLabel.setLayoutY(135);
+            errorLabel.setLayoutY(135); // Adjusted layout for error label
             mainPane.getChildren().removeIf(node -> node instanceof Label);
             mainPane.getChildren().add(errorLabel);
         }
     }
 
-    // plot a linear function based on user-provided values
+    // Method to plot a linear function
     private void plotLine() {
         try {
             double slope = Double.parseDouble(slopeField.getText());
             double intercept = Double.parseDouble(interceptField.getText());
 
             XYChart.Series<Number, Number> series = new XYChart.Series<>();
+
+            // Plotting the graph for x values from -10 to 10
             for (double x = -10; x <= 10; x += 0.1) {
                 double y = slope * x + intercept;
                 series.getData().add(new XYChart.Data<>(x, y));
@@ -169,22 +156,26 @@ public class GraphingCalculator extends Application {
             chart.getData().clear();
             chart.getData().add(series);
         } catch (NumberFormatException ex) {
+            // Handle invalid input
             Label errorLabel = new Label("Please enter valid numbers for slope and intercept.");
             errorLabel.setLayoutX(20);
-            errorLabel.setLayoutY(135);
+            errorLabel.setLayoutY(135); // Adjusted layout for error label
             mainPane.getChildren().removeIf(node -> node instanceof Label);
             mainPane.getChildren().add(errorLabel);
         }
     }
 
-    // plot a parabolic function based on user-provided values
+    // Method to plot a parabolic function
     private void plotParabola() {
         try {
-            double a = Double.parseDouble(slopeField.getText());
-            double b = Double.parseDouble(interceptField.getText());
-            double c = Double.parseDouble(constantField.getText());
+            double a = Double.parseDouble(slopeField.getText()); // Coefficient of x^2
+            double b = Double.parseDouble(interceptField.getText()); // Coefficient of x
+            double c = Double.parseDouble(constantField.getText()); // Constant
+
 
             XYChart.Series<Number, Number> series = new XYChart.Series<>();
+
+            // Plotting the graph for x values from -10 to 10
             for (double x = -10; x <= 10; x += 0.1) {
                 double y = a * x * x + b * x + c;
                 series.getData().add(new XYChart.Data<>(x, y));
@@ -193,21 +184,23 @@ public class GraphingCalculator extends Application {
             chart.getData().clear();
             chart.getData().add(series);
         } catch (NumberFormatException ex) {
+            // Handle invalid input
             Label errorLabel = new Label("Please enter valid numbers for coefficients.");
             errorLabel.setLayoutX(20);
-            errorLabel.setLayoutY(135);
+            errorLabel.setLayoutY(135); // Adjusted layout for error label
             mainPane.getChildren().removeIf(node -> node instanceof Label);
             mainPane.getChildren().add(errorLabel);
         }
     }
 
-    // reset the graph and input fields based on the selected function type
+    // Method to reset the graph and input fields
     private void resetGraph() {
         chart.getData().clear();
         slopeField.clear();
         interceptField.clear();
         constantField.clear();
 
+        // Set initial prompt texts based on the selected function type
         if (linearRadioButton.isSelected()) {
             slopeField.setPromptText("Enter slope (m)");
             interceptField.setPromptText("Enter y-intercept (b)");
