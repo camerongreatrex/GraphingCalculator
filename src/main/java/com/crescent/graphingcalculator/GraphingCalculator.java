55f2a1f258f84d450ebb5199a3f4905aad242444
Julian Chen <127447888+JulianwChen@users.noreply.github.com>
2023-12-01 14:49:35 -0500

    added cams graph to the OG calculator

--- src/main/java/com/crescent/graphingcalculator/GraphingCalculator.java
+++ src/main/java/com/crescent/graphingcalculator/GraphingCalculator.java
@@ -1,37 +1,56 @@
 package com.crescent.graphingcalculator;
 
+import javafx.animation.PauseTransition;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.ScatterChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
+
 
 public class GraphingCalculator extends Application {
     // stage renamed to window
     public static Stage window;
     // pane renamed to mainPane
-    public static Pane mainPane = new Pane();
+    public static Pane calcPane = new Pane();
     //create the main menu scene
-    public static Scene menuScene = new Scene(mainPane, 1000, 700);
+    public static Scene menuScene = new Scene(calcPane, 1000, 800);
     //create buttons for the calculator
     public static Button startCalcButton = new Button("▷"), closeCalcButton = new Button("X");
     //create the groups for the calculator
     public static Group graphGroup = new Group();
     //create the scenes for the calculator
-    public static Scene graphScene = new Scene(graphGroup, 1000, 700, Color.WHITE);
+    public static Scene calcScene = new Scene(graphGroup, 1000, 800, Color.WHITE);
     //create the groups for the buttons screen
     public static Group buttonsGroup = new Group();
+    
+    
+    //create scene for the graph
+    public static Pane graphPane = new Pane();
+    public static Scene graphScene = new Scene(graphPane,1000, 800, Color.WHITE);
+    // Text fields, labels, chart, radio buttons, and the main pane for UI elements
+    private TextField field1, field2, field3, field4;
+    private Label formulaDisplay;
+    private LineChart<Number, Number> chart;
+    private RadioButton linearRadioButton, absoluteRadioButton, parabolaRadioButton, squarerootRadioButton, cubicRadioButton;
+
+
     //create buttons for numbers 0-9 and decimal point
     public static Button one = new Button("1"), two = new Button("2"), three = new Button("3"), four = new Button("4"), five = new Button("5"),
             six = new Button("6"), seven = new Button("7"), eight = new Button("8"), nine = new Button("9"), zero = new Button("0"),
@@ -50,8 +69,6 @@ public class GraphingCalculator extends Application {
 
     private double lastMouseX, lastMouseY;
 
-    //control variable for graphing(y=) textboxs
-    public static boolean graphcheck = false;
 
     @Override
     public void start(Stage stage) throws IOException {
@@ -65,7 +82,7 @@ public class GraphingCalculator extends Application {
         //set properties for the calculator start button
         startCalcButton.setLayoutX(400);
         startCalcButton.setLayoutY(250);
-        mainPane.getChildren().add(startCalcButton);
+        calcPane.getChildren().add(startCalcButton);
         startCalcButton.setMinWidth(200);
         startCalcButton.setMinHeight(100);
         startCalcButton.setStyle(
@@ -73,7 +90,7 @@ public class GraphingCalculator extends Application {
         //set properties for the calculator close button
         closeCalcButton.setLayoutX(400);
         closeCalcButton.setLayoutY(375);
-        mainPane.getChildren().add(closeCalcButton);
+        calcPane.getChildren().add(closeCalcButton);
         closeCalcButton.setMinWidth(200);
         closeCalcButton.setMinHeight(100);
         closeCalcButton.setStyle(
@@ -130,12 +147,9 @@ public class GraphingCalculator extends Application {
 
         //set function of buttons
     {
-        //make all buttons work for different textboxes
         one.setOnMousePressed(startButtonEvent -> {
-            if (!graphcheck) {
             textbox.setText(textbox.getText() + 1);
             textbox.positionCaret(textbox.getText().length());
-            }
         });
         two.setOnMousePressed(startButtonEvent -> {
             textbox.setText(textbox.getText() + 2);
@@ -240,18 +254,14 @@ public class GraphingCalculator extends Application {
         });
     }
     {
-        
+        //graph button will toggle on/off graphing feature
         graph.setOnAction(startButtonEvent -> {
-            if (graphcheck) {
-                textbox.setVisible(true);
-            } else {
-                textbox.setVisible(false);
-            }
-            graphcheck = !graphcheck;
+            textbox.setText("");
+            stage.setScene(graphScene);
         });
     }  
         startCalcButton.setOnAction(startButtonEvent -> {
-            window.setScene(graphScene);
+            window.setScene(calcScene);
             //create number axes
             final NumberAxis xAxis = new NumberAxis(-500, 500, 100);
             final NumberAxis yAxis = new NumberAxis(-500, 500, 100);
@@ -290,11 +300,139 @@ public class GraphingCalculator extends Application {
         closeCalcButton.setOnAction(closeButtonEvent -> {
             window.close();
         });
+
+
+
+        stage.setTitle("Graphing Calculator");
+
+        stage.setResizable(false);
+
+        // Create X and Y axes for the chart
+        NumberAxis xAxis = new NumberAxis();
+        NumberAxis yAxis = new NumberAxis();
+        xAxis.setLabel("x");
+        yAxis.setLabel("y");
+
+        // Create the line chart to display the graph
+        chart = new LineChart<>(xAxis, yAxis);
+        chart.setLayoutX(20);
+        chart.setLayoutY(150);
+        chart.setPrefSize(600, 600);
+        chart.setLegendVisible(false);
+        chart.setAnimated(false);
+
+        // Text fields for user input
+        field1 = new TextField();
+        field1.setLayoutX(20);
+        field1.setLayoutY(20);
+        field1.setPrefWidth(180);
+
+        field2 = new TextField();
+        field2.setLayoutX(20);
+        field2.setLayoutY(60);
+        field2.setPrefWidth(180);
+
+        field3 = new TextField();
+        field3.setLayoutX(20);
+        field3.setLayoutY(100);
+        field3.setPrefWidth(180);
+
+        field4 = new TextField();
+        field4.setLayoutX(210);
+        field4.setLayoutY(20);
+        field4.setPrefWidth(180);
+
+        // Label to display the formula of the selected function
+        formulaDisplay = new Label("FORMULA: y = mx + b");
+        formulaDisplay.setLayoutX(20);
+
+        // Radio buttons to select the function type
+        linearRadioButton = new RadioButton("Linear Function");
+        linearRadioButton.setSelected(true);
+        linearRadioButton.setLayoutX(400);
+        linearRadioButton.setLayoutY(20);
+
+        absoluteRadioButton = new RadioButton("Absolute Function");
+        absoluteRadioButton.setLayoutX(550);
+        absoluteRadioButton.setLayoutY(60);
+
+        parabolaRadioButton = new RadioButton("Parabola");
+        parabolaRadioButton.setLayoutX(400);
+        parabolaRadioButton.setLayoutY(60);
+
+        squarerootRadioButton = new RadioButton("Square Root");
+        squarerootRadioButton.setLayoutX(400);
+        squarerootRadioButton.setLayoutY(100);
+
+        cubicRadioButton = new RadioButton("Cubic");
+        cubicRadioButton.setLayoutX(550);
+        cubicRadioButton.setLayoutY(20);
+
+        ToggleGroup functionToggleGroup = new ToggleGroup();
+        linearRadioButton.setToggleGroup(functionToggleGroup);
+        absoluteRadioButton.setToggleGroup(functionToggleGroup);
+        parabolaRadioButton.setToggleGroup(functionToggleGroup);
+        squarerootRadioButton.setToggleGroup(functionToggleGroup);
+        cubicRadioButton.setToggleGroup(functionToggleGroup);
+
+        // Buttons for plotting and resetting the graph
+        Button plotGraphButton = new Button("Plot Graph");
+        plotGraphButton.setLayoutX(700);
+        plotGraphButton.setLayoutY(180);
+        plotGraphButton.setOnAction(e -> plotGraph());
+
+        Button resetButton = new Button("Reset Graph");
+        resetButton.setLayoutX(700);
+        resetButton.setLayoutY(220);
+        resetButton.setOnAction(e -> resetGraph());
+
+        Button backToCalc = new Button("Back");
+        backToCalc.setLayoutX(700);
+        backToCalc.setLayoutY(260);
+        backToCalc.setOnAction(startButtonEvent -> {
+            resetGraph();
+            stage.setScene(calcScene);
+        });
+
+        // Adding all elements to the main pane
+        graphPane.getChildren().addAll(chart, field1, field2, field3, field4, linearRadioButton, absoluteRadioButton,
+                parabolaRadioButton, squarerootRadioButton, cubicRadioButton, formulaDisplay, plotGraphButton, resetButton, backToCalc);
+
+        // Set the default to linear function and organize fields accordingly
+        linearRadioButton.setSelected(true);
+        field3.setVisible(false);
+        resetGraph();
+
+        // Event listeners to update the UI based on the selected function
+        linearRadioButton.setOnAction(e -> {
+            formulaDisplay.setText("FORMULA: y = mx + b");
+            organizeFields("Enter slope (m)", "Enter y-intercept (b)", null, null);
+            resetGraph();
+        });
+        absoluteRadioButton.setOnAction(e -> {
+            formulaDisplay.setText("FORMULA: y = |a * x|");
+            organizeFields("Enter coefficient (a)", null, null, null);
+            resetGraph();
+        });
+        parabolaRadioButton.setOnAction(e -> {
+            formulaDisplay.setText("FORMULA: y = ax^2 + bx + c");
+            organizeFields("Enter coefficient (a)", "Enter constant (b)", "Enter constant (c)", null);
+            resetGraph();
+        });
+        squarerootRadioButton.setOnAction(e -> {
+            formulaDisplay.setText("FORMULA: y = a√b(x - h) + k");
+            organizeFields("Enter coefficient (a)", "Enter constant (b)", null, null);
+            resetGraph();
+        });
+
+        cubicRadioButton.setOnAction(e -> {
+            formulaDisplay.setText("FORMULA: y = ax^3 + bx^2 + cx + d");
+            organizeFields("Enter coefficient (a)", "Enter coefficient (b)", "Enter coefficient (c)", "Enter coefficient (d)");
+            resetGraph();
+        });
+
     }
 
-    // void checkTextbox() {
-    //     if (graphchecks)
-    // }
 
     //method for setting up number buttons
     public void setButton(Button button, int width, int height, double x, double y) {
@@ -367,6 +505,218 @@ public class GraphingCalculator extends Application {
             lastMouseX = event.getX();
             lastMouseY = event.getY();
         });
+    }
+
+
+
+    private void organizeFields(String prompt1, String prompt2, String prompt3, String prompt4) {
+        field1.setPromptText(prompt1);
+        field2.setPromptText(prompt2);
+        field3.setPromptText(prompt3);
+        field4.setPromptText(prompt4);
+        if (prompt2 != null) {
+            field2.setVisible(true);
+            field2.setPromptText(prompt2);
+        } else {
+            field2.setVisible(false);
+            field2.clear();
+        }
+        if (prompt3 != null) {
+            field3.setVisible(true);
+            field3.setPromptText(prompt3);
+        } else {
+            field3.setVisible(false);
+            field3.clear();
+        }
+        if (prompt4 != null) {
+            field4.setVisible(true);
+        } else {
+            field4.setVisible(false);
+            field4.clear();
+        }
+
+    }
+
+    // Method to plot the selected graph (linear, parabolic, square root, cubic)
+    private void plotGraph() {
+        if (linearRadioButton.isSelected()) {
+            plotLine();
+        } else if (absoluteRadioButton.isSelected()) {
+            plotAbsoluteFunction();
+        } else if (parabolaRadioButton.isSelected()) {
+            plotParabola();
+        } else if (squarerootRadioButton.isSelected()) {
+            plotSquareRoot();
+        } else if (cubicRadioButton.isSelected()) {
+            plotCubicFunction();
+        }
+    }
+
+    // Method to plot a linear function
+    private void plotLine() {
+        try {
+            double slope = Double.parseDouble(field1.getText());
+            double intercept = Double.parseDouble(field2.getText());
+
+            XYChart.Series<Number, Number> series = new XYChart.Series<>();
+
+            for (double x = -10; x <= 10; x += 0.5) {
+                double y = slope * x + intercept;
+                series.getData().add(new XYChart.Data<>(x, y));
+            }
+
+            chart.getData().clear();
+            chart.getData().add(series);
+        } catch (NumberFormatException ex) {
+            Label errorLabel = new Label("Please enter valid numbers.");
+            errorLabel.setLayoutX(20);
+            errorLabel.setLayoutY(135);
+            graphPane.getChildren().removeIf(node -> node instanceof Label && !node.equals(formulaDisplay));
+            graphPane.getChildren().add(errorLabel);
+            // Hide the error label after 3 seconds
+            PauseTransition visiblePause = new PauseTransition(Duration.seconds(1.5));
+            visiblePause.setOnFinished(event -> graphPane.getChildren().remove(errorLabel));
+            visiblePause.play();
+        }
+    }
+
+    // Method to plot an absolute function
+    private void plotAbsoluteFunction() {
+        try {
+            double a = Double.parseDouble(field1.getText());
+
+            XYChart.Series<Number, Number> series = new XYChart.Series<>();
+
+            for (double x = -10; x <= 10; x += 0.5) {
+                double y = Math.abs(a * x);
+                series.getData().add(new XYChart.Data<>(x, y));
+            }
+
+            chart.getData().clear();
+            chart.getData().add(series);
+        } catch (NumberFormatException ex) {
+            Label errorLabel = new Label("Please enter a valid number for coefficient (a).");
+            errorLabel.setLayoutX(20);
+            errorLabel.setLayoutY(135);
+            graphPane.getChildren().removeIf(node -> node instanceof Label && !node.equals(formulaDisplay));
+            graphPane.getChildren().add(errorLabel);
+            // Hide the error label after 3 seconds
+            PauseTransition visiblePause = new PauseTransition(Duration.seconds(1.5));
+            visiblePause.setOnFinished(event -> graphPane.getChildren().remove(errorLabel));
+            visiblePause.play();
+        }
+    }
+
+    // Method to plot a parabolic function
+    private void plotParabola() {
+        try {
+            double a = Double.parseDouble(field1.getText());
+            double b = Double.parseDouble(field2.getText());
+            double c = Double.parseDouble(field3.getText());
+
+            // Calculate the x-coordinate of the vertex to fix the bug of printing more on one quadrant of another
+            double vertexX = -b / (2 * a);
+
+            XYChart.Series<Number, Number> series = new XYChart.Series<>();
+
+            for (double x = vertexX - 20; x <= vertexX + 20; x += 0.1) {
+                double y = a * x * x + b * x + c;
+                series.getData().add(new XYChart.Data<>(x, y));
+            }
+
+            chart.getData().clear();
+            chart.getData().add(series);
+        } catch (NumberFormatException ex) {
+            Label errorLabel = new Label("Please enter valid numbers");
+            errorLabel.setLayoutX(20);
+            errorLabel.setLayoutY(135);
+            graphPane.getChildren().removeIf(node -> node instanceof Label && !node.equals(formulaDisplay));
+            graphPane.getChildren().add(errorLabel);
+            // Hide the error label after 3 seconds
+            PauseTransition visiblePause = new PauseTransition(Duration.seconds(1.5));
+            visiblePause.setOnFinished(event -> graphPane.getChildren().remove(errorLabel));
+            visiblePause.play();
+        }
+    }
+
+    // Method to plot a square root function
+    private void plotSquareRoot() {
+        try {
+            double a = Double.parseDouble(field1.getText());
+            double b = Double.parseDouble(field2.getText());
+
+            XYChart.Series<Number, Number> series = new XYChart.Series<>();
+
+            for (double x = 0; x <= 10; x += 0.5) {
+                double y = a * Math.sqrt(x) + b;
+                series.getData().add(new XYChart.Data<>(x, y));
+            }
+
+            chart.getData().clear();
+            chart.getData().add(series);
+        } catch (NumberFormatException ex) {
+            Label errorLabel = new Label("Please enter valid numbers.");
+            errorLabel.setLayoutX(20);
+            errorLabel.setLayoutY(135);
+            graphPane.getChildren().removeIf(node -> node instanceof Label && !node.equals(formulaDisplay));
+            graphPane.getChildren().add(errorLabel);
+            // Hide the error label after 3 seconds
+            PauseTransition visiblePause = new PauseTransition(Duration.seconds(1.5));
+            visiblePause.setOnFinished(event -> graphPane.getChildren().remove(errorLabel));
+            visiblePause.play();
+        }
+    }
+
+    // Method to plot a cubic function
+    private void plotCubicFunction() {
+        try {
+            double a = Double.parseDouble(field1.getText());
+            double b = Double.parseDouble(field2.getText());
+            double c = Double.parseDouble(field3.getText());
+            double d = Double.parseDouble(field4.getText());
+
+            XYChart.Series<Number, Number> series = new XYChart.Series<>();
+
+            for (double x = -10; x <= 10; x += 0.5) {
+                double y = a * Math.pow(x, 3) + b * Math.pow(x, 2) + c * x + d;
+                series.getData().add(new XYChart.Data<>(x, y));
+            }
+
+            chart.getData().clear();
+            chart.getData().add(series);
+        } catch (NumberFormatException ex) {
+            Label errorLabel = new Label("Please enter valid numbers.");
+            errorLabel.setLayoutX(20);
+            errorLabel.setLayoutY(175);
+            graphPane.getChildren().removeIf(node -> node instanceof Label && !node.equals(formulaDisplay));
+            graphPane.getChildren().add(errorLabel);
+            // Hide the error label after 3 seconds
+            PauseTransition visiblePause = new PauseTransition(Duration.seconds(1.5));
+            visiblePause.setOnFinished(event -> graphPane.getChildren().remove(errorLabel));
+            visiblePause.play();
+        }
+    }
+
+
+    // Method to reset the graph and input fields
+    private void resetGraph() {
+        chart.getData().clear();
+        field1.clear();
+        field2.clear();
+        field3.clear();
+        field4.clear();
+
+        if (linearRadioButton.isSelected()) {
+            organizeFields("Enter slope (m)", "Enter y-intercept (b)", null, null);
+        } else if (absoluteRadioButton.isSelected()) {
+            organizeFields("Enter coefficient (a)", null, null, null);
+        } else if (parabolaRadioButton.isSelected()) {
+            organizeFields("Enter coefficient (a)", "Enter constant (b)", "Enter constant (c)", null);
+        } else if (squarerootRadioButton.isSelected()) {
+            organizeFields("Enter coefficient (a)", "Enter constant (b)", null, null);
+        } else if (cubicRadioButton.isSelected()) {
+            organizeFields("Enter coefficient (a)", "Enter coefficient (b)", "Enter coefficient (c)", "Enter coefficient (d)");
+        }
     }
 
     public static void main(String[] args) {
